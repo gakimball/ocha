fefe378c9268f6f32bce47b72c892180ac22c1b7
Geoff Kimball <geoff@zurb.com>
2016-03-04 00:34:53 -0800

    Get basics of method to recreate True's assertions working

--- test/test.js
+++ test/test.js
@@ -1,10 +1,15 @@
+var eyeglass = require('eyeglass');
 var sass = require('node-sass');
+var True = require('sass-true');
 
-describe('Ocha', () => {
-  it('works', done => {
-    sass.render({ file: 'test/test.scss' }, function(err, data) {
-      if (err) console.log(err.message);
-      done();
-    });
-  });
-});
+True.runSass({ file: 'test/test.scss' }, describe, it);
+
+// describe('Ocha', () => {
+//   it('works', done => {
+//     sass.render(eyeglass({ file: 'test/test.scss' }), function(err, data) {
+//       if (err) console.log(err.message);
+//       console.log(data.css.toString());
+//       done();
+//     });
+//   });
+// });
