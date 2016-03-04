4ae9745d8c9688c43eb1ec493b693ecb7aabb7a4
Geoff Kimball <geoff@zurb.com>
2016-03-04 13:15:59 -0800

    Get equal assertion working with True, and set up easier testing framework

--- test/test.js
+++ test/test.js
@@ -1,15 +1,21 @@
 var eyeglass = require('eyeglass');
+var expect = require('chai').expect;
 var sass = require('node-sass');
 var True = require('sass-true');
 
-True.runSass({ file: 'test/test.scss' }, describe, it);
+var trueParse = require('sass-true/lib/main').parse;
 
-// describe('Ocha', () => {
-//   it('works', done => {
-//     sass.render(eyeglass({ file: 'test/test.scss' }), function(err, data) {
-//       if (err) console.log(err.message);
-//       console.log(data.css.toString());
-//       done();
-//     });
-//   });
-// });
+describe('Ocha', () => {
+  it('works', done => {
+    test('test/test.scss', true, done);
+  });
+});
+
+function test(file, passed, cb) {
+  sass.render(eyeglass({ file: file }), (err, res) => {
+    if (err) throw err;
+    var tests = trueParse(res.css.toString());
+    expect(tests[0].tests[0].assertions[0].passed).to.equal(passed);
+    cb();
+  });
+}
